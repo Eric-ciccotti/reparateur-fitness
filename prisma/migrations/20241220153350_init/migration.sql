862fcd8a12ad988edc8749c025be8d3a7fa8545f
Eric-ciccotti <eric.ciccotti@gmail.com>
2024-12-20 16:46:55 +0100

    chore: clean up env files and build directory

--- prisma/migrations/20241220153350_init/migration.sql
+++ prisma/migrations/20241220153350_init/migration.sql
@@ -1,15 +1,17 @@
 -- CreateTable
 CREATE TABLE "Booking" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL,
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "time" TEXT NOT NULL,
     "equipmentType" TEXT NOT NULL,
     "message" TEXT,
     "status" TEXT NOT NULL DEFAULT 'PENDING',
-    "payment_id" TEXT
+    "payment_id" TEXT,
+
+    CONSTRAINT "Booking_pkey" PRIMARY KEY ("id")
 );
